9acbda8b0e58b7d1aa7d044dc6f04be898d6c6e9
Heiner Schmidt <heiner.schmidt@gmail.com>
2026-02-18 17:39:11 +0100

    wrote a proper main, including some error handling and basic command argument processing

--- planeted/main.cpp
+++ planeted/main.cpp
@@ -1,19 +1,66 @@
 #include <iostream>
+#include <unistd.h>
+#include <getopt.h>
+#include <cstdlib>
 
 #include "Vector3.h"
 #include "Mesh.h"
 #include "POV.h"
 
-using namespace std;
 using namespace Planeted;
 
-int main()
+char *filename = nullptr;
+bool readargs(int argc, char** argv);
+
+void usage();
+
+int main(int argc, char **argv)
 {
+    if(readargs(argc, argv) == false)
+    {
+        usage();
+        return EXIT_FAILURE; //error;
+    }
+
+    /*
     Mesh mesh;
     Vector3* test = new Vector3(3.141, 0.0, 0.0);
-    cout << "Hello world!" << endl;
-    cout << POVR::Vector3ToPOV(test) << endl;
-    cout << POVR::MeshToPOVMesh2(&mesh) << endl;
-    cout << POVR::POVSceneFile("model.pov") << endl;
-    return 0;
+
+    std::cout << POVR::Vector3ToPOV(test) << std::endl;
+    std::cout << POVR::MeshToPOVMesh2(&mesh) << std::endl;
+    std::cout << POVR::POVSceneFile(filename) << std::endl;
+    */
+
+    return EXIT_SUCCESS;
+}
+
+bool readargs(int argc, char **argv)
+{
+    int c;
+    int longindex;
+    bool result = false;
+
+    struct option longopts[] =
+    {
+        {"filename", required_argument, NULL, 'f'},
+        {NULL, 0, NULL, 0}
+    };
+
+    while((c = getopt_long(argc, argv, "f:", longopts, &longindex)) != -1)
+    {
+        switch(c)
+        {
+        case 'f':
+            filename = optarg;
+            result = true;
+            break;
+        }
+    }
+    return result;
+}
+
+void usage()
+{
+    std::cout << "usage: planeted [-f | --filename = <outputfile>]\n\n";
+    std::cout << "\t planeted is a tool for procedurally creating and editing models of asteroids, moons and small planets." << std::endl;
 }
