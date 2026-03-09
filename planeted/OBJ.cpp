a976b4717f689721778dc93b7956906b75b51552
Heiner Schmidt <heiner.schmidt@gmail.com>
2026-03-09 15:11:36 +0100

    removed unneeded forward declarations from header file; helper methods now take references instead of pointers as arguments

--- planeted/OBJ.cpp
+++ planeted/OBJ.cpp
@@ -8,34 +8,34 @@ namespace Planeted
 {
     namespace OBJ
     {
-        std::string Vector3ToOBJ(Vector3 *vector3)
+        std::string Vertex3ToOBJ(Vector3& vector3)
         {
             std::stringstream stream;
 
             stream << std::fixed << std::setprecision(4);
-            stream << "v " << vector3->X() << " " << vector3->Y() << " " << vector3->Z();
+            stream << "v " << vector3.X() << " " << vector3.Y() << " " << vector3.Z();
 
             return stream.str();
         }
 
-        std::string NormalToOBJ(Vector3 *normal)
+        std::string NormalToOBJ(Vector3& normal)
         {
             std::stringstream stream;
 
             stream << std::fixed << std::setprecision(4);
-            stream << "vn " << normal->X() << " " << normal->Y() << " " << normal->Z();
+            stream << "vn " << normal.X() << " " << normal.Y() << " " << normal.Z();
 
             return stream.str();
         }
 
-        std::string TriangleIndicesToOBJ(TriangleIndices *indices)
+        std::string TriangleIndicesToOBJ(TriangleIndices& indices)
         {
             std::stringstream stream;
 
             stream << "f ";
-            stream << indices->v0 + 1 << "//" << indices->n + 1 << " ";
-            stream << indices->v1 + 1 << "//" << indices->n + 1<< " ";
-            stream << indices->v2 + 1 << "//" << indices->n + 1;
+            stream << indices.v0 + 1 << "//" << indices.n + 1 << " ";
+            stream << indices.v1 + 1 << "//" << indices.n + 1<< " ";
+            stream << indices.v2 + 1 << "//" << indices.n + 1;
 
             return stream.str();
         }
@@ -46,19 +46,19 @@ namespace Planeted
 
             for(Vector3 v : mesh.Vertices())
             {
-                stream << Vector3ToOBJ(&v) << "\n";
+                stream << Vertex3ToOBJ(v) << "\n";
             }
             stream << "\n";
 
             for(Vector3 n : mesh.Normals())
             {
-                stream << NormalToOBJ(&n) << "\n";
+                stream << NormalToOBJ(n) << "\n";
             }
             stream << "\n";
 
             for(TriangleIndices i : mesh.Triangles())
             {
-                stream << TriangleIndicesToOBJ(&i) << "\n";
+                stream << TriangleIndicesToOBJ(i) << "\n";
             }
             return stream.str();
         }
--- planeted/OBJ.h
+++ planeted/OBJ.h
@@ -10,10 +10,6 @@ namespace Planeted
 {
     namespace OBJ
     {
-        std::string Vector3ToOBJ(Vector3 *vector3);
-        std::string NormalToOBJ(Vector3 *normal);
-        std::string TriangleIndicesToOBJ(TriangleIndices *indices);
-
         std::string MeshToOBJ(Mesh& mesh);
     }
 }
