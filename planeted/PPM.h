d392200687fc20ab111ce33cda438aae7de89a52
Heiner Schmidt <heiner.schmidt@gmail.com>
2026-03-17 18:57:28 +0100

    basic ppm exporter

---
+++ planeted/PPM.h
@@ -0,0 +1,38 @@
+#ifndef PLANETED_PPM_H
+#define PLANETED_PPM_H
+
+#include <sstream>
+#include <string.h>
+#include <vector>
+
+#include "PixelMap.h"
+
+namespace Planeted
+{
+    namespace PPM
+    {
+        std::string PixelMapToPPM(const PixelMap& pmap)
+        {
+            std::stringstream stream;
+
+            int width = pmap.GetWidth();
+            int height = pmap.GetHeight();
+            const std::vector<Color>& data = pmap.GetData();
+
+            stream << "P3\n";
+            stream << width << " " << height << "\n";
+            stream << "255\n";
+
+            for (int i = 0; i < width * height; ++i)
+            {
+                const Color& c = data[i];
+
+                stream << static_cast<int>(c.R) << " ";
+                stream << static_cast<int>(c.G) << " "
+                stream << static_cast<int>(c.B) << "\n";
+            }
+            return stream.str();
+        }
+    }
+}
+#endif // PLANETED_PPM_H
