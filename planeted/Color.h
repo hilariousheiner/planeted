9d6995ee6802e35b91f76564522ad22d131dabaf
Heiner Schmidt <heiner.schmidt@gmail.com>
2026-03-17 18:54:11 +0100

    added struct for storing 8 bit rgb colors

---
+++ planeted/Color.h
@@ -0,0 +1,22 @@
+#ifndef PLANETED_COLOR_H
+#define PLANETED_COLOR_H
+
+#include <cstdint>
+
+struct Color
+{
+    std::uint8_t R;
+    std::uint8_t G;
+    std::uint8_t B;
+};
+
+namespace Colors
+{
+    static constexpr Color Black {0  ,   0,   0};
+    static constexpr Color Red   {255,   0,   0};
+    static constexpr Color Green {0  , 255,   0};
+    static constexpr Color Blue  {0  ,   0, 255};
+    static constexpr Color White {255, 255, 255};
+}
+
+#endif // PLANETED_COLOR_H
