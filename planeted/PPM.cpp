6672ee15e63046df2b4480152141610be0866e56
Heiner Schmidt <heiner.schmidt@gmail.com>
2026-03-19 22:22:33 +0100

    moved implementation of ppm exporter to ppm.cpp

---
+++ planeted/PPM.cpp
@@ -0,0 +1,28 @@
+#include "PPM.h"
+
+namespace Planeted
+{
+    namespace PPM
+    {
+        std::string PixelMapToPPM(const PixelMap& pmap)
+        {
+            std::stringstream stream;
+
+            int width = pmap.GetWidth();
+            int height = pmap.GetHeight();
+            const std::vector<Color>& data = pmap.GetData();
+
+            stream << "P3\n";
+            stream << width << " " << height << "\n";
+            stream << "255\n";
+
+            for (const Color& pixel : data)
+            {
+                stream << static_cast<int>(pixel.R) << " ";
+                stream << static_cast<int>(pixel.G) << " ";
+                stream << static_cast<int>(pixel.B) << "\n";
+            }
+            return stream.str();
+        }
+    }
+}
--- planeted/PPM.h
+++ planeted/PPM.h
@@ -11,26 +11,7 @@ namespace Planeted
 {
     namespace PPM
     {
-        std::string PixelMapToPPM(const PixelMap& pmap)
-        {
-            std::stringstream stream;
-
-            int width = pmap.GetWidth();
-            int height = pmap.GetHeight();
-            const std::vector<Color>& data = pmap.GetData();
-
-            stream << "P3\n";
-            stream << width << " " << height << "\n";
-            stream << "255\n";
-
-            for (const Color& pixel : data)
-            {
-                stream << static_cast<int>(pixel.R) << " ";
-                stream << static_cast<int>(pixel.G) << " ";
-                stream << static_cast<int>(pixel.B) << "\n";
-            }
-            return stream.str();
-        }
+        std::string PixelMapToPPM(const PixelMap& pmap);
     }
 }
 #endif // PLANETED_PPM_H
