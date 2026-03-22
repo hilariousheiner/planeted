0cfa65206feb73f74dd2c421c77d0f5655fbbcc6
Heiner Schmidt <heiner.schmidt@gmail.com>
2026-03-22 19:24:00 +0100

    added missing namespace

--- planeted/Color.h
+++ planeted/Color.h
@@ -3,20 +3,23 @@
 
 #include <cstdint>
 
-struct Color
+namespace Planeted
 {
-    std::uint8_t R;
-    std::uint8_t G;
-    std::uint8_t B;
-};
+    struct Color
+    {
+        std::uint8_t R;
+        std::uint8_t G;
+        std::uint8_t B;
+    };
 
-namespace Colors
-{
-    static constexpr Color Black {0  ,   0,   0};
-    static constexpr Color Red   {255,   0,   0};
-    static constexpr Color Green {0  , 255,   0};
-    static constexpr Color Blue  {0  ,   0, 255};
-    static constexpr Color White {255, 255, 255};
+    namespace Colors
+    {
+        static constexpr Color Black {0  ,   0,   0};
+        static constexpr Color Red   {255,   0,   0};
+        static constexpr Color Green {0  , 255,   0};
+        static constexpr Color Blue  {0  ,   0, 255};
+        static constexpr Color White {255, 255, 255};
+    }
 }
-
 #endif // PLANETED_COLOR_H
+
