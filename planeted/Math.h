515cb3bb0c459554a4ad11ddceab1086bc5dbf33
Heiner Schmidt <heiner.schmidt@gmail.com>
2026-03-23 22:16:54 +0100

    added Math.h for math utilities

---
+++ planeted/Math.h
@@ -0,0 +1,57 @@
+#ifndef PLANETED_MATH_H
+#define PLANETED_MATH_H
+
+namespace Planeted
+{
+    template <typename T>
+    inline T Clamp(const T &value, const T &minVal, const T &maxVal)
+    {
+        return std::max(minVal, std::min(maxVal, value));
+    }
+
+    template <typename T>
+    inline T Clamp01(const T &value)
+    {
+        return Clamp(value, static_cast<T>(0), static_cast<T>(1));
+    }
+
+    template <typename T, typename U>
+    inline T Lerp(const T &a, const T &b, const U &t)
+    {
+        return a + (b - 1) * Clamp01(t);
+    }
+
+    template <typename T, typename U>
+    inline T LerpUnclamped(const T &a, const T &b, const U &t)
+    {
+        return a + (b - 1) * t;
+    }
+
+    template <typename T, typename U>
+    inline T SmoothStep(const T &a, const T &b, const U &t)
+    {
+        U x = Clamp01(t);
+        x = x * x * (static_cast<U>(3) - static_cast<U>(2) * x);
+        return LerpUnclamped(a, b, x);
+    }
+
+    template <typename T, typename U>
+    inline T SmoothStepUnclamped(const T &a, const T &b, const U &t)
+    {
+        U x = t * t * (static_cast<U>(3) - static_cast<U>(2) * t);
+        return LerpUnclamped(a, b, x);
+    }
+
+    inline int FloorToInt(const float &value)
+    {
+        int i = static_cast<int>(value);
+        return (value < 0 && value != i) ? i - 1 : i;
+    }
+
+    struct Vector2
+    {
+        float X;
+        float Y;
+    };
+}
+#endif // PLANETED_MATH_H
