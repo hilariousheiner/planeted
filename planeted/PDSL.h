b0a898fb1b73e24da62a545cf794fab6dcbf0a4e
Heiner Schmidt <heiner.schmidt@gmail.com>
2026-05-09 20:00:32 +0200

    initial commit for planeted scripting language

---
+++ planeted/PDSL.h
@@ -0,0 +1,362 @@
+#ifndef PLANETED_PDSL_H
+#define PLANETED_PDSL_H
+
+#include <stdexcept>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+/*
+ * PDSL = planeted scripting language
+ */
+
+namespace Planeted
+{
+    enum class TokenTypeEnum
+    {
+        Identifier,
+
+        Int,
+        Float,
+        Bool,
+
+        Keyword,
+
+        Equals,
+        Semicolon,
+
+        End
+    };
+
+    struct Token
+    {
+        TokenTypeEnum type;
+        std::string lexeme;
+    };
+
+    std::string TokenTypeToString(TokenTypeEnum tokenType)
+    {
+        std::string result = "unknown token type";
+        switch(tokenType)
+        {
+        case TokenTypeEnum::Identifier:
+            result = "Identifier";
+            break;
+        case TokenTypeEnum::Int:
+            result = "Int";
+            break;
+        case TokenTypeEnum::Float:
+            result = "Float";
+            break;
+        case TokenTypeEnum::Bool:
+            result = "Bool";
+            break;
+        case TokenTypeEnum::Keyword:
+            result = "Keyword";
+            break;
+        case TokenTypeEnum::Equals:
+            result = "Equals";
+            break;
+        case TokenTypeEnum::Semicolon:
+            result = "Semicolon";
+            break;
+        case TokenTypeEnum::End:
+            result = "End";
+            break;
+        default:
+            break;
+        }
+        return result;
+    }
+
+    static const std::unordered_map<std::string, bool> keywordDict =
+    {
+        {"true", true},
+        {"false", true}
+    };
+
+    class Lexer
+    {
+    public:
+        Lexer(const std::string &source);
+        Token Next();
+
+    private:
+        void readWhitespace();
+        Token readIdentifier();
+        Token readNumber();
+        char peek() const;
+        char advance();
+
+        std::string source;
+        size_t pos;
+    };
+
+    Lexer::Lexer(const std::string &source) :
+        source(source), pos(0)
+    { }
+
+
+    Token Lexer::Next()
+    {
+        this->readWhitespace();
+
+        char c = this->peek();
+
+        if(c == '\0')
+        {
+            return {TokenTypeEnum::End, ""};
+        }
+
+        if(std::isalpha(c) || c == '_')
+        {
+            return this->readIdentifier();
+        }
+
+        if(std::isdigit(c))
+        {
+            return this->readNumber();
+        }
+
+        this->advance();
+
+        switch(c)
+        {
+        case '=':
+            return {TokenTypeEnum::Equals, "="};
+            break;
+        case ';':
+            return {TokenTypeEnum::Semicolon, "="};
+        default:
+            throw std::runtime_error("unexpected character: " + c);
+            break;
+        }
+    }
+
+    void Lexer::readWhitespace()
+    {
+        while(std::isspace(this->peek()))
+        {
+            this->advance();
+        }
+    }
+
+    Token Lexer::readIdentifier()
+    {
+        std::string text;
+
+        while (std::isalnum(this->peek()) || this->peek() == '_')
+        {
+            text += this->advance();
+        }
+
+        std::unordered_map<std::string, bool>::const_iterator it;
+        it = keywordDict.find(text);
+        if(it != keywordDict.end())
+        {
+            return {TokenTypeEnum::Keyword, text};
+        }
+        return {TokenTypeEnum::Identifier, text};
+    }
+
+    Token Lexer::readNumber()
+    {
+        std::string text;
+        bool isFloat = false;
+
+        while(std::isdigit(this->peek()) || this->peek() == '.')
+        {
+            if(this->peek() == '.')
+            {
+                if(isFloat)
+                {
+                    throw std::runtime_error("Invalid float.");
+                }
+                isFloat = true;
+            }
+            text += this->advance();
+        }
+
+        if(isFloat)
+        {
+            return {TokenTypeEnum::Float, text};
+        }
+        return {TokenTypeEnum::Int, text};
+    }
+
+    char Lexer::peek() const
+    {
+        if(this->pos >= this->source.size())
+        {
+            return '\0';
+        }
+        return this->source[pos];
+    }
+
+    char Lexer::advance()
+    {
+        return this->source[this->pos++];
+    }
+
+    enum class ValueTypeEnum
+    {
+        Identifier,
+        Int,
+        Float,
+        Bool
+    };
+
+    struct Value
+    {
+        ValueTypeEnum valueType;
+
+        int IntValue;
+        float FloatValue;
+        bool BoolValue;
+        std::string Identifier;
+    };
+
+    struct Statement
+    {
+        std::string name;
+        Value value;
+    };
+
+    struct Program
+    {
+        std::vector<Statement> statements;
+    };
+
+    class Parser
+    {
+    public:
+        Parser(Lexer &lexer) :
+            lexer(lexer)
+        {
+            this->advance();
+        }
+
+        Program Parse()
+        {
+            Program result;
+
+            while(this->current.type != TokenTypeEnum::End)
+            {
+                result.statements.push_back(this->parseStatement());
+            }
+            return result;
+        }
+
+    private:
+
+        Statement parseStatement()
+        {
+            Statement result;
+
+            //identifier
+            result.name = this->expect(TokenTypeEnum::Identifier).lexeme;
+
+            // =
+            this->expect(TokenTypeEnum::Equals);
+
+            // value
+            result.value = this->parseValue();
+
+            //;
+            this->expect(TokenTypeEnum::Semicolon);
+
+            return result;
+        }
+
+        Value parseValue()
+        {
+            Value result;
+
+            switch(this->current.type)
+            {
+            case TokenTypeEnum::Identifier:
+                result.valueType = ValueTypeEnum::Identifier;
+                result.Identifier = current.lexeme;
+                break;
+            case TokenTypeEnum::Int:
+                result.valueType = ValueTypeEnum::Int;
+                result.IntValue = std::stoi(current.lexeme);
+                break;
+            case TokenTypeEnum::Float:
+                result.valueType = ValueTypeEnum::Float;
+                result.FloatValue = std::stof(current.lexeme);
+                break;
+            case TokenTypeEnum::Bool:
+                result.valueType = ValueTypeEnum::Bool;
+                result.BoolValue = (current.lexeme == "true");
+                break;
+            default:
+                throw std::runtime_error("Invalid value type");
+            }
+
+            this->advance();
+
+            return result;
+        }
+
+        void advance()
+        {
+            this->current = this->lexer.Next();
+        }
+
+        Token expect(TokenTypeEnum tokenType)
+        {
+            if(this->current.type != tokenType)
+            {
+                throw std::runtime_error("Unexpected token: " + TokenTypeToString(this->current.type) + " (expected " + TokenTypeToString(tokenType) + ")");
+            }
+
+            Token result = current;
+            this->advance();
+
+            return result;
+        }
+
+        Token current;
+        Lexer &lexer;
+    };
+
+    struct Runtime
+    {
+        std::unordered_map<std::string, Value> Environment;
+
+        Value GetVariableValue(const std::string &name) const
+        {
+            std::unordered_map<std::string, Value>::const_iterator it = this->Environment.find(name);
+            if(it == this->Environment.end())
+            {
+                throw std::runtime_error("Undefined variable: " + name);
+            }
+            return it->second;
+        }
+
+        void SetVariableValue(std::string name, Value value)
+        {
+            this->Environment[name] = value;
+        }
+    };
+
+    Value evalValue(const Value &value, Runtime &runtime)
+    {
+        if(value.valueType == ValueTypeEnum::Identifier)
+        {
+            return runtime.GetVariableValue(value.Identifier);
+        }
+        return value;
+    }
+
+    void Run(const Program &program, Runtime &runtime)
+    {
+        for(const Statement &stmt : program.statements)
+        {
+            Value value = evalValue(stmt.value, runtime);
+            runtime.SetVariableValue(stmt.name, value);
+        }
+    }
+}
+#endif
