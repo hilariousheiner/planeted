60ad9a0a0878012f207ae8d7c96b7c79f1d83fe8
Heiner Schmidt <heiner.schmidt@gmail.com>
2026-05-16 20:03:59 +0200

    moved implementation of Parser to PDSL_Parser.cpp

--- planeted/PDSL_Parser.cpp
+++ planeted/PDSL_Parser.cpp
@@ -2,5 +2,219 @@
 
 namespace Planeted
 {
+    ConstantExpression::ConstantExpression(Value value)
+        : value(value)
+    {}
 
+    Value ConstantExpression::eval(PDSL_Runtime &runtime)
+    {
+        return value;
+    }
+
+    VariableExpression::VariableExpression(std::string identifier)
+        : Identifier(identifier)
+    {}
+
+    Value VariableExpression::eval(PDSL_Runtime &runtime)
+    {
+        return runtime.GetVariableValue(this->Identifier);
+    }
+
+    AssignmentStatement::AssignmentStatement(std::string name, std::unique_ptr<Expression> expression)
+        : name(name), expression(std::move(expression))
+    {}
+
+    void AssignmentStatement::execute(PDSL_Runtime &runtime)
+    {
+        runtime.SetVariableValue(this->name, this->expression->eval(runtime));
+    }
+
+    ImportStatement::ImportStatement(std::string path)
+        : path(path) {}
+
+    void ImportStatement::execute(PDSL_Runtime &runtime)
+    {
+        PDSL_Run(ReadFile(this->path), runtime);
+    }
+
+    FunctionCallStatement::FunctionCallStatement(std::string name, std::vector<std::unique_ptr<Expression>> args)
+        : name(name), args(std::move(args))
+    {}
+
+    void FunctionCallStatement::execute(PDSL_Runtime &runtime)
+    {
+        auto it = runtime.BuiltinFunctionsTable.find(this->name);
+        if(it == runtime.BuiltinFunctionsTable.end())
+        {
+            throw std::runtime_error("Unknown function: " + this->name);
+        }
+
+        std::vector<Value> evaluatedArgs;
+
+        for(const auto & arg : this->args)
+        {
+            evaluatedArgs.push_back(arg->eval(runtime));
+        }
+
+        it->second(runtime, evaluatedArgs);
+    }
+
+    Parser::Parser(Lexer &lexer)
+        : lexer(lexer)
+    {
+        this->next = this->lexer.Next();
+        this->advance();
+    }
+
+    Program Parser::Parse()
+    {
+        Program result;
+
+        while(this->current.type != TokenTypeEnum::End)
+        {
+            result.statements.push_back(this->parseStatement());
+        }
+        return result;
+    }
+
+    std::unique_ptr<Statement> Parser::parseStatement()
+    {
+        std::unique_ptr<Statement> result;
+
+        if(this->current.type == TokenTypeEnum::Import)
+        {
+            result = this->parseImportStatement();
+        }
+        else
+        {
+            if(this->next.type == TokenTypeEnum::LParen)
+            {
+                result = this->parseFunctionCallStatement();
+            }
+            else
+            {
+                result = this->parseAssignmentStatement();
+            }
+        }
+        return result;
+    }
+
+    std::unique_ptr<ImportStatement> Parser::parseImportStatement()
+    {
+        this->expect(TokenTypeEnum::Import);
+
+        std::string path = this->expect(TokenTypeEnum::StringLiteral).lexeme;
+
+        return std::make_unique<ImportStatement>(path);
+    }
+
+    std::unique_ptr<AssignmentStatement> Parser::parseAssignmentStatement()
+    {
+        // identifier
+        std::string name = this->expect(TokenTypeEnum::Identifier).lexeme;
+
+        // =
+        this->expect(TokenTypeEnum::Equals);
+
+        // expression
+        std::unique_ptr<Expression> expression = this->parseExpression();
+
+        // ;
+        this->expect(TokenTypeEnum::Semicolon);
+
+        return std::make_unique<AssignmentStatement>(name, std::move(expression));
+    }
+
+    std::unique_ptr<FunctionCallStatement> Parser::parseFunctionCallStatement()
+    {
+        // identifier
+        std::string name = this->expect(TokenTypeEnum::Identifier).lexeme;
+
+        // parse argument list:
+        // (
+        this->expect(TokenTypeEnum::LParen);
+
+        std::vector<std::unique_ptr<Expression>> args;
+
+        if(this->current.type != TokenTypeEnum::RParen)
+        {
+            while(true)
+            {
+                args.push_back(this->parseExpression());
+                if(this->current.type == TokenTypeEnum::Comma)
+                {
+                    this->advance();
+                    continue;
+                }
+                break;
+            }
+        }
+        // )
+
+        this->expect(TokenTypeEnum::RParen);
+        // ;
+        this->expect(TokenTypeEnum::Semicolon);
+        return std::make_unique<FunctionCallStatement>(name, std::move(args));
+    }
+
+    std::unique_ptr<Expression> Parser::parseExpression()
+    {
+        if(this->current.type == TokenTypeEnum::Identifier)
+        {
+            std::string identifier = this->current.lexeme;
+            this->advance();
+            return std::make_unique<VariableExpression>(identifier);
+        }
+        return this->parseLiteral();
+    }
+
+    std::unique_ptr<ConstantExpression> Parser::parseLiteral()
+    {
+        Value result;
+
+        switch(this->current.type)
+        {
+        case TokenTypeEnum::IntLiteral:
+            result.valueType = ValueTypeEnum::Int;
+            result.IntValue = std::stoi(current.lexeme);
+            break;
+        case TokenTypeEnum::FloatLiteral:
+            result.valueType = ValueTypeEnum::Float;
+            result.FloatValue = std::stof(current.lexeme);
+            break;
+        case TokenTypeEnum::BoolLiteral:
+            result.valueType = ValueTypeEnum::Bool;
+            result.BoolValue = (current.lexeme == "true");
+            break;
+        case TokenTypeEnum::StringLiteral:
+            result.valueType = ValueTypeEnum::String;
+            result.StringValue = current.lexeme;
+            break;
+        default:
+            throw std::runtime_error("Invalid value type: " + TokenTypeToString(this->current.type));
+            break;
+        }
+
+        this->advance();
+        return std::make_unique<ConstantExpression>(result);
+    }
+
+    void Parser::advance()
+    {
+        this->current = this->next;
+        this->next = this->lexer.Next();
+    }
+
+    Token Parser::expect(TokenTypeEnum tokenType)
+    {
+        if(this->current.type != tokenType)
+        {
+            throw std::runtime_error("Unexpected token: " + TokenTypeToString(this->current.type) + " (expected " + TokenTypeToString(tokenType) + ")");
+        }
+
+        Token result = current;
+        this->advance();
+
+        return result;
+    }
 }
--- planeted/PDSL_Parser.h
+++ planeted/PDSL_Parser.h
@@ -30,36 +30,25 @@ namespace Planeted
     {
         Value value;
 
-        ConstantExpression(Value value) : value(value) {}
+        ConstantExpression(Value value);
 
-        Value eval(PDSL_Runtime &runtime) override
-        {
-            return value;
-        }
+        Value eval(PDSL_Runtime &runtime) override;
     };
 
     struct VariableExpression : Expression
     {
         std::string Identifier;
 
-        VariableExpression(std::string identifier) : Identifier(identifier) {}
+        VariableExpression(std::string identifier);
 
-        Value eval(PDSL_Runtime &runtime) override
-        {
-            return runtime.GetVariableValue(this->Identifier);
-        }
+        Value eval(PDSL_Runtime &runtime) override;
     };
 
     struct AssignmentStatement : Statement
     {
-        AssignmentStatement(std::string name, std::unique_ptr<Expression> expression) :
-            name(name), expression(std::move(expression))
-        { }
+        AssignmentStatement(std::string name, std::unique_ptr<Expression> expression);
 
-        virtual void execute(PDSL_Runtime &runtime) override
-        {
-            runtime.SetVariableValue(this->name, this->expression->eval(runtime));
-        }
+        void execute(PDSL_Runtime &runtime) override;
 
         std::string name;
         std::unique_ptr<Expression> expression;
@@ -67,39 +56,18 @@ namespace Planeted
 
     struct ImportStatement : Statement
     {
-        ImportStatement(std::string path) :
-            path(path) {}
+        ImportStatement(std::string path);
 
-        virtual void execute(PDSL_Runtime &runtime) override
-        {
-            PDSL_Run(ReadFile(this->path), runtime);
-        }
+        void execute(PDSL_Runtime &runtime) override;
 
         std::string path;
     };
 
     struct FunctionCallStatement : Statement
     {
-        FunctionCallStatement(std::string name, std::vector<std::unique_ptr<Expression>> args) :
-            name(name), args(std::move(args)) {}
+        FunctionCallStatement(std::string name, std::vector<std::unique_ptr<Expression>> args);
 
-        virtual void execute(PDSL_Runtime &runtime) override
-        {
-            auto it = runtime.BuiltinFunctionsTable.find(this->name);
-            if(it == runtime.BuiltinFunctionsTable.end())
-            {
-                throw std::runtime_error("Unknown function: " + this->name);
-            }
-
-            std::vector<Value> evaluatedArgs;
-
-            for(const auto & arg : this->args)
-            {
-                evaluatedArgs.push_back(arg->eval(runtime));
-            }
-
-            it->second(runtime, evaluatedArgs);
-        }
+        void execute(PDSL_Runtime &runtime) override;
 
         std::string name;
         std::vector<std::unique_ptr<Expression>> args;
@@ -108,167 +76,27 @@ namespace Planeted
     class Parser
     {
     public:
-        Parser(Lexer &lexer) :
-            lexer(lexer)
-        {
-            this->next = this->lexer.Next();
-            this->advance();
-        }
+        Parser(Lexer &lexer);
 
-        Program Parse()
-        {
-            Program result;
-
-            while(this->current.type != TokenTypeEnum::End)
-            {
-                result.statements.push_back(this->parseStatement());
-            }
-            return result;
-        }
+        Program Parse();
 
     private:
 
-        std::unique_ptr<Statement> parseStatement()
-        {
-            std::unique_ptr<Statement> result;
+        std::unique_ptr<Statement> parseStatement();
 
-            if(this->current.type == TokenTypeEnum::Import)
-            {
-                result = this->parseImportStatement();
-            }
-            else
-            {
-                if(this->next.type == TokenTypeEnum::LParen)
-                {
-                    result = this->parseFunctionCallStatement();
-                }
-                else
-                {
-                    result = this->parseAssignmentStatement();
-                }
-            }
-            return result;
-        }
+        std::unique_ptr<ImportStatement> parseImportStatement();
 
-        std::unique_ptr<ImportStatement> parseImportStatement()
-        {
-            this->expect(TokenTypeEnum::Import);
+        std::unique_ptr<AssignmentStatement> parseAssignmentStatement();
 
-            std::string path = this->expect(TokenTypeEnum::StringLiteral).lexeme;
+        std::unique_ptr<FunctionCallStatement> parseFunctionCallStatement();
 
-            return std::make_unique<ImportStatement>(path);
-        }
+        std::unique_ptr<Expression> parseExpression();
 
-        std::unique_ptr<AssignmentStatement> parseAssignmentStatement()
-        {
-            // identifier
-            std::string name = this->expect(TokenTypeEnum::Identifier).lexeme;
+        std::unique_ptr<ConstantExpression> parseLiteral();
 
-            // =
-            this->expect(TokenTypeEnum::Equals);
+        void advance();
 
-            // expression
-            std::unique_ptr<Expression> expression = this->parseExpression();
-
-            // ;
-            this->expect(TokenTypeEnum::Semicolon);
-
-            return std::make_unique<AssignmentStatement>(name, std::move(expression));
-        }
-
-        std::unique_ptr<FunctionCallStatement> parseFunctionCallStatement()
-        {
-            // identifier
-            std::string name = this->expect(TokenTypeEnum::Identifier).lexeme;
-
-            // parse argument list:
-            // (
-            this->expect(TokenTypeEnum::LParen);
-
-            std::vector<std::unique_ptr<Expression>> args;
-
-            if(this->current.type != TokenTypeEnum::RParen)
-            {
-                while(true)
-                {
-                    args.push_back(this->parseExpression());
-                    if(this->current.type == TokenTypeEnum::Comma)
-                    {
-                        this->advance();
-                        continue;
-                    }
-                    break;
-                }
-            }
-            // )
-
-            this->expect(TokenTypeEnum::RParen);
-            // ;
-            this->expect(TokenTypeEnum::Semicolon);
-            return std::make_unique<FunctionCallStatement>(name, std::move(args));
-        }
-
-        std::unique_ptr<Expression> parseExpression()
-        {
-            if(this->current.type == TokenTypeEnum::Identifier)
-            {
-                std::string identifier = this->current.lexeme;
-                this->advance();
-                return std::make_unique<VariableExpression>(identifier);
-            }
-            return this->parseLiteral();
-        }
-
-        std::unique_ptr<ConstantExpression> parseLiteral()
-        {
-            Value result;
-
-            switch(this->current.type)
-            {
-            case TokenTypeEnum::IntLiteral:
-                result.valueType = ValueTypeEnum::Int;
-                result.IntValue = std::stoi(current.lexeme);
-                break;
-            case TokenTypeEnum::FloatLiteral:
-                result.valueType = ValueTypeEnum::Float;
-                result.FloatValue = std::stof(current.lexeme);
-                break;
-            case TokenTypeEnum::BoolLiteral:
-                result.valueType = ValueTypeEnum::Bool;
-                result.BoolValue = (current.lexeme == "true");
-                break;
-            case TokenTypeEnum::StringLiteral:
-                result.valueType = ValueTypeEnum::String;
-                result.StringValue = current.lexeme;
-                break;
-            default:
-                throw std::runtime_error("Invalid value type: " + TokenTypeToString(this->current.type));
-                break;
-            }
-
-            this->advance();
-
-            return std::make_unique<ConstantExpression>(result);
-        }
-
-        void advance()
-        {
-            this->current = this->next;
-            this->next = this->lexer.Next();
-        }
-
-        Token expect(TokenTypeEnum tokenType)
-        {
-            if(this->current.type != tokenType)
-            {
-                throw std::runtime_error("Unexpected token: " + TokenTypeToString(this->current.type) + " (expected " + TokenTypeToString(tokenType) + ")");
-            }
-
-            Token result = current;
-            this->advance();
-
-            return result;
-        }
+        Token expect(TokenTypeEnum tokenType);
 
         Token current;
         Token next;
