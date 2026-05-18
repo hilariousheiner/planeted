15fe066ceabea6da3346b873ad5cb392aa4aa8cd
Heiner Schmidt <heiner.schmidt@gmail.com>
2026-05-18 14:13:42 +0200

    moved implementation of Value to PSDL_Value.cpp

--- planeted/PDSL_Value.cpp
+++ planeted/PDSL_Value.cpp
@@ -2,5 +2,119 @@
 
 namespace Planeted
 {
+    Value::Value()
+        : data(std::monostate{})
+    {}
 
+    Value::Value(int intValue)
+        : data(intValue)
+    {}
+
+    Value::Value(float floatValue)
+        : data(floatValue)
+    {}
+
+    Value::Value(bool boolValue)
+        : data(boolValue)
+    {}
+
+    Value::Value(std::string stringValue)
+        : data(stringValue)
+    {}
+
+    Value::Value(Mesh *meshValue)
+        : data(meshValue)
+    {}
+
+    bool Value::IsNull() const
+    {
+        return std::holds_alternative<std::monostate>(this->data);
+    }
+
+    const Value &Value::Null()
+    {
+        static const Value null{};
+        return null;
+    }
+
+    int Value::GetIntValue() const
+    {
+        return std::get<int>(this->data);
+    }
+    float Value::GetFloatValue() const
+    {
+        return std::get<float>(this->data);
+    }
+    bool Value::GetBoolValue() const
+    {
+        return std::get<bool>(this->data);
+    }
+    std::string Value::GetStringValue() const
+    {
+        return std::get<std::string>(this->data);
+    }
+    Mesh *Value::GetMeshValue() const
+    {
+        return std::get<Mesh*>(this->data);
+    }
+
+    ValueTypeEnum Value::GetValueType() const
+    {
+        if(std::holds_alternative<int>(this->data))
+        {
+            return ValueTypeEnum::Int;
+        }
+        if(std::holds_alternative<float>(this->data))
+        {
+            return ValueTypeEnum::Float;
+        }
+        if(std::holds_alternative<bool>(this->data))
+        {
+            return ValueTypeEnum::Bool;
+        }
+
+        if(std::holds_alternative<std::string>(this->data))
+        {
+            return ValueTypeEnum::String;
+        }
+        if(std::holds_alternative<std::monostate>(this->data))
+        {
+            return ValueTypeEnum::Null;
+        }
+        if(std::holds_alternative<Mesh*>(this->data))
+        {
+            return ValueTypeEnum::Mesh;
+        }
+        return ValueTypeEnum::Null; // Todo: maybe throw an exception here?
+    }
+
+    std::string Value::ToString() const
+    {
+        std::string result = "";
+
+        switch(this->GetValueType())
+        {
+        case ValueTypeEnum::Int:
+            result = std::to_string(std::get<int>(this->data));
+            break;
+        case ValueTypeEnum::Float:
+            result = std::to_string(std::get<float>(this->data));
+            break;
+        case ValueTypeEnum::Bool:
+            result = std::get<bool>(this->data) == true ? "true" : "false";
+            break;
+        case ValueTypeEnum::String:
+            result = std::get<std::string>(this->data);
+            break;
+        case ValueTypeEnum::Null:
+            result = "null";
+            break;
+        case ValueTypeEnum::Mesh:
+            result = "mesh: " + std::get<Mesh*>(this->data)->GetName();
+            break;
+        default:
+            break;
+        }
+        return result;
+    }
 }
--- planeted/PDSL_Value.h
+++ planeted/PDSL_Value.h
@@ -20,103 +20,25 @@ namespace Planeted
     class Value
     {
     public:
-        Value() : data(std::monostate{}) {}
-        Value(int intValue) : data(intValue) {}
-        Value(float floatValue) : data(floatValue) {}
-        Value(bool boolValue) : data(boolValue) {}
-        Value(std::string stringValue) : data(stringValue) {}
-        Value(Mesh *meshValue) : data(meshValue) {}
+        Value();
+        Value(int intValue);
+        Value(float floatValue);
+        Value(bool boolValue);
+        Value(std::string stringValue);
+        Value(Mesh *meshValue);
 
-        bool IsNull() const
-        {
-            return std::holds_alternative<std::monostate>(this->data);
-        }
-        static const Value &Null()
-        {
-            static const Value null{};
-            return null;
-        }
+        bool IsNull() const;
+        static const Value &Null();
 
-        int GetIntValue() const
-        {
-            return std::get<int>(this->data);
-        }
-        float GetFloatValue() const
-        {
-            return std::get<float>(this->data);
-        }
-        bool GetBoolValue() const
-        {
-            return std::get<bool>(this->data);
-        }
-        std::string GetStringValue() const
-        {
-            return std::get<std::string>(this->data);
-        }
-        Mesh *GetMeshValue() const
-        {
-            return std::get<Mesh*>(this->data);
-        }
+        int GetIntValue() const;
+        float GetFloatValue() const;
+        bool GetBoolValue() const;
+        std::string GetStringValue() const;
+        Mesh *GetMeshValue() const;
 
-        ValueTypeEnum GetValueType() const
-        {
-            if(std::holds_alternative<int>(this->data))
-            {
-                return ValueTypeEnum::Int;
-            }
-            if(std::holds_alternative<float>(this->data))
-            {
-                return ValueTypeEnum::Float;
-            }
-            if(std::holds_alternative<bool>(this->data))
-            {
-                return ValueTypeEnum::Bool;
-            }
+        ValueTypeEnum GetValueType() const;
 
-            if(std::holds_alternative<std::string>(this->data))
-            {
-                return ValueTypeEnum::String;
-            }
-            if(std::holds_alternative<std::monostate>(this->data))
-            {
-                return ValueTypeEnum::Null;
-            }
-            if(std::holds_alternative<Mesh*>(this->data))
-            {
-                return ValueTypeEnum::Mesh;
-            }
-            return ValueTypeEnum::Null; // Todo: maybe throw an exception here?
-        }
-
-        std::string ToString() const
-        {
-            std::string result = "";
-
-            switch(this->GetValueType())
-            {
-            case ValueTypeEnum::Int:
-                result = std::to_string(std::get<int>(this->data));
-                break;
-            case ValueTypeEnum::Float:
-                result = std::to_string(std::get<float>(this->data));
-                break;
-            case ValueTypeEnum::Bool:
-                result = std::get<bool>(this->data) == true ? "true" : "false";
-                break;
-            case ValueTypeEnum::String:
-                result = std::get<std::string>(this->data);
-                break;
-            case ValueTypeEnum::Null:
-                result = "null";
-                break;
-            case ValueTypeEnum::Mesh:
-                result = "mesh: " + std::get<Mesh*>(this->data)->GetName();
-                break;
-            default:
-                break;
-            }
-            return result;
-        }
+        std::string ToString() const;
 
     private:
         std::variant<std::monostate, int, float, bool, std::string, Mesh*> data;
