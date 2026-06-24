837cdefd60bf9929109b01212d448a1e7fb986f8
Heiner Schmidt <heiner.schmidt@gmail.com>
2026-06-24 11:07:58 +0200

    code cleanup

--- planeted/OBJ.cpp
+++ planeted/OBJ.cpp
@@ -18,18 +18,6 @@ namespace Planeted
             return stream.str();
         }
 
-        /*
-        std::string normalToOBJ(const Vector3 &normal)
-        {
-            std::stringstream stream;
-
-            stream << std::fixed << std::setprecision(4);
-            stream << "vn " << normal.X << " " << normal.Y << " " << normal.Z;
-
-            return stream.str();
-        }
-        */
-
         std::string triangleIndicesToOBJ(const TriangleIndices &indices, NormalTypeEnum normalType)
         {
             std::stringstream stream;
@@ -39,27 +27,6 @@ namespace Planeted
             stream << indices.V1 + 1 << " ";
             stream << indices.V2 + 1;
 
-            /*
-            switch(normalType)
-            {
-                case NormalTypeEnum::PerFace:
-                    stream << indices.V0 + 1 << "//" << indices.N + 1 << " ";
-                    stream << indices.V1 + 1 << "//" << indices.N + 1<< " ";
-                    stream << indices.V2 + 1 << "//" << indices.N + 1;
-                    break;
-                case NormalTypeEnum::PerVertex:
-                    stream << indices.V0 + 1 << "//" << indices.V0 + 1 << " ";
-                    stream << indices.V1 + 1 << "//" << indices.V1 + 1<< " ";
-                    stream << indices.V2 + 1 << "//" << indices.V2 + 1;
-                    break;
-                case NormalTypeEnum::None:
-                default:
-                    stream << indices.V0 + 1 << " ";
-                    stream << indices.V1 + 1 << " ";
-                    stream << indices.V2 + 1;
-                    break;
-            }
-            */
             return stream.str();
         }
 
@@ -72,14 +39,6 @@ namespace Planeted
                 stream << vertex3ToOBJ(v) << "\n";
             }
             stream << "\n";
-
-            /*
-            for(Vector3 n : mesh.Normals())
-            {
-                stream << normalToOBJ(n) << "\n";
-            }
-            stream << "\n";
-            */
 
             for(TriangleIndices i : mesh.Triangles())
             {
