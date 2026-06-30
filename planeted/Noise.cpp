4c69f9d05ed47d39d08dbb3e3251c22583cb24fe
Heiner Schmidt <heiner.schmidt@gmail.com>
2026-06-30 22:58:11 +0200

    added Noise.cpp and moved implementation there from Noise.h

---
+++ planeted/Noise.cpp
@@ -0,0 +1,34 @@
+#include "Noise.h"
+#include "PRNG.h"
+
+namespace Planeted
+{
+    Noise::Noise()
+    {
+        this->seed = StringToSeed32("Planeted");
+        this->seed64 = StringToSeed64("Planeted");
+    }
+    Noise::Noise(std::string seed)
+    {
+        this->seed = StringToSeed32(seed);
+        this->seed64 = StringToSeed64(seed);
+    }
+    Noise::Noise(std::uint32_t seed)
+    {
+        this->seed = seed;
+        this->seed64 = FMix64(static_cast<uint64_t>(seed));
+    };
+
+    float Noise::Noise1D(const float &p) const
+    {
+        return Random::FBM1D(p, this->fbmParams, this->noiseParams, Random::GetNoiseFunction1D(this->noiseType, this->noiseStyle));
+    }
+    float Noise::Noise2D(const Vector2 &p) const
+    {
+        return Random::FBM2D(p, this->fbmParams, this->noiseParams, Random::GetNoiseFunction2D(this->noiseType, this->noiseStyle));
+    }
+    float Noise::Noise3D(const Vector3 &p) const
+    {
+        return Random::FBM3D(p, this->fbmParams, this->noiseParams, Random::GetNoiseFunction3D(this->noiseType, this->noiseStyle));
+    }
+}
--- planeted/Noise.h
+++ planeted/Noise.h
@@ -2,7 +2,6 @@
 #define PLANETED_NOISE_H
 
 #include "Random.h"
-#include "PRNG.h"
 
 namespace Planeted
 {
@@ -27,34 +26,5 @@ namespace Planeted
         std::uint32_t seed;
         std::uint64_t seed64;
     };
-
-    Noise::Noise()
-    {
-        this->seed = StringToSeed32("Planeted");
-        this->seed64 = StringToSeed64("Planeted");
-    }
-    Noise::Noise(std::string seed)
-    {
-        this->seed = StringToSeed32(seed);
-        this->seed64 = StringToSeed64(seed);
-    }
-    Noise::Noise(std::uint32_t seed)
-    {
-        this->seed = seed;
-        this->seed64 = FMix64(static_cast<uint64_t>(seed));
-    };
-
-    float Noise::Noise1D(const float &p) const
-    {
-        return Random::FBM1D(p, this->fbmParams, this->noiseParams, Random::GetNoiseFunction1D(this->noiseType, this->noiseStyle));
-    }
-    float Noise::Noise2D(const Vector2 &p) const
-    {
-        return Random::FBM2D(p, this->fbmParams, this->noiseParams, Random::GetNoiseFunction2D(this->noiseType, this->noiseStyle));
-    }
-    float Noise::Noise3D(const Vector3 &p) const
-    {
-        return Random::FBM3D(p, this->fbmParams, this->noiseParams, Random::GetNoiseFunction3D(this->noiseType, this->noiseStyle));
-    }
 }
 #endif
